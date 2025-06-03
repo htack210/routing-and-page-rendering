95bbb7b6afe71395c2c23900eecfce07437602f8
Glenn Haertlein <htack210@live.com>
2025-06-03 17:54:22 -0400

    Add not found pages

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Well, this is embarrassing!</h1>
+      <h2>The article you are seeking was not found!</h2>
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Well, this is embarrassing!</h1>
+      <h2>The page you are seeking was not found!</h2>
+    </div>
+  );
+}
